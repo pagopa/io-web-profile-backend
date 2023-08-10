f2143a968d256d1929a7f5b9be3950266465e945
USERSAD\EDALLOC3I <claudio11.dallospedale@nttdata.com>
2023-08-10 10:08:58 +0200

    USing other Create Client Overload for HUB SPID LOGIN

--- clients/hubSpidLogin.ts
+++ clients/hubSpidLogin.ts
@@ -1,27 +1,21 @@
 import nodeFetch from "node-fetch";
-import { INonEmptyStringTag } from "@pagopa/ts-commons/lib/strings";
 import {
   Client,
   createClient
 } from "../generated/definitions/hub-spid-login/client";
 
-const tokenX: string & INonEmptyStringTag = "A" as string & INonEmptyStringTag;
+// const tokenX: string & INonEmptyStringTag = "A" as string & INonEmptyStringTag;
 export const getHubSpidLoginClient = (
   baseUrl: string,
   basePath?: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   fetchApi: typeof fetch = (nodeFetch as any) as typeof fetch
-): Client<"body"> =>
+): Client<"_____"> =>
   createClient({
     basePath,
     baseUrl,
-    fetchApi,
+    fetchApi
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    withDefaults: op => params =>
-      op({
-        ...params,
-        body: { token: tokenX }
-      })
   });
 
 export type GetHubSpidLoginClient = typeof getHubSpidLoginClient;
