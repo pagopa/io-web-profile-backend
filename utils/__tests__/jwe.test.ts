3d3c6b610a622a5627f404cfd7e184e6f72de917
pelliccm <matteo.pelliccione@nttdata.com>
2023-09-18 12:14:28 +0200

    [IOPID-632] magic link ok

--- utils/__tests__/jwe.test.ts
+++ utils/__tests__/jwe.test.ts
@@ -1,4 +1,5 @@
 import * as E from "fp-ts/Either";
+import * as jose from "jose";
 
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { Second } from "@pagopa/ts-commons/lib/units";
@@ -6,27 +7,20 @@ import { Second } from "@pagopa/ts-commons/lib/units";
 import { getGenerateJWE } from "../jwe";
 
 describe(`getGenerateJWE`, () => {
-  it(
-    "should return a valid JWE",
-    async () => {
-      const issuer = "PAGOPA" as NonEmptyString;
-      const jweKey = `-----BEGIN PRIVATE KEY-----
+  it("should return a valid JWE", async () => {
+    const jwe =
+      "eyJhbGciOiJFQ0RILUVTK0EyNTZLVyIsImNydiI6IlAtMjU2IiwiZW5jIjoiQTEyOENCQy1IUzI1NiIsImt0eSI6IkVDIiwiZXBrIjp7IngiOiJvYk83YWhGelA5b0xvckhWYjhxRG5oRVZRenpEejZPd2NKbjk0b25MeUhvIiwiY3J2IjoiUC0yNTYiLCJrdHkiOiJFQyIsInkiOiJIV2hWV2RyUF9PLVA2VTZRVGxLY3NtQXVSRW92NDduZ0lVWjJrcXJYVjZFIn19.xzz0VtYGwuyKjfQQ9KfJR0cLKGXPL9D6UHlR5s4YDNsHTsIBCXvzAg.9pu8FSaOvq9xGBSL8zeUzw.-f8zCJHuuXZ6H2wME5RuSk981afu0uqhPuP-JHYZ2jIp_lUht3Mhhx3PTCWR0CwW-sQqpqiz1SlXfr3cGJhy8KMSmCptKCK_e6zpzJJ5KVdBox5BTZTamqY6MbLP417aZW0GXKtAr9dl2ji40hqWThu7d-xTkvMz19ItyBxfedM.DulzGzH0vgqG7N0t6RL7pw";
+    const jweKey = `-----BEGIN PRIVATE KEY-----
     MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgiyvo0X+VQ0yIrOaN
     nlrnUclopnvuuMfoc8HHly3505OhRANCAAQWUcdZ8uTSAsFuwtNy4KtsKqgeqYxg
     l6kwL5D4N3pEGYGIDjV69Sw0zAt43480WqJv7HCL0mQnyqFmSrxj8jMa
     -----END PRIVATE KEY-----` as NonEmptyString;
-      const payload = { field: "test" };
 
-      const result = await getGenerateJWE(issuer, jweKey)(
-        payload,
-        900 as Second
-      )();
+    const pkcs8 = await jose.importPKCS8(jweKey, "ECDH-ES+A256KW");
+    const result = await jose.compactDecrypt(jwe, pkcs8);
 
-      expect(E.isRight(result)).toBeTruthy();
-      if (E.isRight(result)) {
-        expect(result.right).toBe("");
-      }
-    },
-    700000000
-  );
+    const expected =
+      '{"name":"Carla","family_name":"Rossi","fiscal_code":"ISPXNB32R82Y766D","iss":"pagopa","iat":1695031301,"exp":1695636101}';
+    expect(new TextDecoder().decode(result.plaintext)).toBe(expected);
+  });
 });
--- utils/jwe.ts
+++ utils/jwe.ts
@@ -1,6 +1,6 @@
 import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
-import { pipe } from "fp-ts/lib/function";
+import { flow, pipe } from "fp-ts/lib/function";
 import * as jose from "jose";
 
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
@@ -14,40 +14,44 @@ export type GetGenerateJWE = <T extends Record<string, unknown>>(
   jweKey: NonEmptyString
 ) => (payload: T, ttl: Second) => TE.TaskEither<Error, NonEmptyString>;
 
+const secondsFromEpoch = (secondsToAdd: number): Second => {
+  const date = new Date();
+  const today = Math.floor(date.getTime() / 1000);
+  const total = today + secondsToAdd;
+  return total as Second;
+};
+
+const alg = "ECDH-ES+A256KW";
 export const getGenerateJWE: GetGenerateJWE = (issuer, jweKey) => (
   payload,
   ttl
 ): TE.TaskEither<Error, NonEmptyString> =>
   pipe(
     TE.tryCatch(
-      () => jose.importPKCS8(jweKey, "ECDH-ES+A256KW"),
+      () => jose.importPKCS8(jweKey, alg),
       e => {
         throw new Error(`Invalid JWE Key. Error: ${e}`);
       }
     ),
-    TE.chain(pkcs8 => {
-      const plaintext = new TextEncoder().encode(
-        JSON.stringify({ ...payload, issuer, ttl })
-      );
-      console.log("plaintext: ", plaintext);
-      return TE.taskify<Error, string>(() =>
-        new jose.GeneralEncrypt(plaintext)
-          .setProtectedHeader({
-            // crv: "P-256",
-            enc: "A128CBC-HS256"
-            // kty: "EC"
-          })
-          .addRecipient(pkcs8)
-          .setUnprotectedHeader({
-            alg: "ECDH-ES+A256KW"
-          })
-          .encrypt()
-      )();
-    }),
-    x => {
-      console.log("====> ", x);
-      return x;
-    },
+    TE.chain(pkcs8 =>
+      TE.tryCatch(
+        () =>
+          new jose.EncryptJWT(payload)
+            .setProtectedHeader({
+              alg,
+              crv: "P-256",
+              enc: "A128CBC-HS256",
+              kty: "EC"
+            })
+            .setIssuer(issuer)
+            .setIssuedAt()
+            .setExpirationTime(secondsFromEpoch(ttl))
+            .encrypt(pkcs8),
+        e => {
+          throw new Error(`Cannot generate JWE. Error: ${e}`);
+        }
+      )
+    ),
     TE.mapLeft(e => E.toError(`Error: ${e}`)),
     TE.chain(
       TE.fromPredicate(NonEmptyString.is, () => E.toError("Token is empty."))
