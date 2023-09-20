b4f98fa57554c15b3bb632b0778c804be3c0936b
pelliccm <matteo.pelliccione@nttdata.com>
2023-09-20 12:26:38 +0200

    Review

--- utils/__tests__/jwe.test.ts
+++ utils/__tests__/jwe.test.ts
@@ -4,22 +4,28 @@ import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { Second } from "@pagopa/ts-commons/lib/units";
 
 import { getGenerateJWE } from "../jwe";
+import { exportPKCS8, generateKeyPair } from "jose";
 
 describe(`getGenerateJWE`, () => {
   it("should return a valid JWE", async () => {
-    const jweKey = `-----BEGIN PRIVATE KEY-----
-    MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgiyvo0X+VQ0yIrOaN
-    nlrnUclopnvuuMfoc8HHly3505OhRANCAAQWUcdZ8uTSAsFuwtNy4KtsKqgeqYxg
-    l6kwL5D4N3pEGYGIDjV69Sw0zAt43480WqJv7HCL0mQnyqFmSrxj8jMa
-    -----END PRIVATE KEY-----` as NonEmptyString;
+    const jweKeys = await generateKeyPair("ECDH-ES+A256KW");
+    const formattedKey = (await exportPKCS8(
+      jweKeys.privateKey
+    )) as NonEmptyString;
+
     const issuer = "pagopa" as NonEmptyString;
     const ttl = 900 as Second;
+
     const aValidPayload = {
       family_name: "fn",
       fiscal_number: "fc",
       name: "n"
     };
-    const result = await getGenerateJWE(issuer, jweKey)(aValidPayload, ttl)();
+
+    const result = await getGenerateJWE(issuer, formattedKey)(
+      aValidPayload,
+      ttl
+    )();
 
     expect(result).toMatchObject(
       E.right(expect.stringMatching(`[A-Za-z0-9-_]{1,520}`))
