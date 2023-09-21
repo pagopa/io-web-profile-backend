c7e5253dbf97c9b785ef8f60ad6adb804daf3b88
pelliccm <matteo.pelliccione@nttdata.com>
2023-09-21 16:05:54 +0200

    PR review

--- utils/enums/TokenTypes.ts
+++ utils/enums/TokenTypes.ts
@@ -1,7 +1,3 @@
-import { ExchangeJwtPayloadExtended } from "../middlewares/exchange-jwt-validation-middleware";
-import { HslJwtPayloadExtended } from "../middlewares/hsl-jwt-validation-middleware";
-
 export enum TokenTypes {
-  EXCHANGE = "exchange",
-  MAGIC_LINK = "magic_link"
+  EXCHANGE = "exchange"
 }
