deca43b5635f6a9a5df230ecf321afc14d6e18f4
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-13 12:22:54 +0200

    add new identities

---
+++ infra/identity/prod/locals.tf
@@ -0,0 +1,18 @@
+locals {
+  prefix    = "io"
+  env_short = "p"
+  env       = "prod"
+  domain    = "web-profile-backend"
+  repo_name = "io-web-profile-backend"
+
+  session_manager_environment = "session-manager-prod"
+
+  tags = {
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "Auth&Identity"
+    Source         = "https://github.com/pagopa/io-web-profile-backend/blob/main/infra/identity/prod"
+  }
+}
