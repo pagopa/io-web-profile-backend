e4c3f4db3547548a9f7a421e4be6fbf06fb41f73
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2024-11-22 17:42:36 +0100

    [#IOPID-1970] Add code review pipeline over github actions (#45)

--- infra/repository/data.tf
+++ infra/repository/data.tf
@@ -7,3 +7,19 @@ data "azurerm_user_assigned_identity" "identity_opex_prod_cd" {
   name                = "${local.project}-web-profile-backend-opex-github-cd-identity"
   resource_group_name = local.identity_resource_group_name
 }
+
+data "github_organization_teams" "all" {
+  root_teams_only = true
+  summary_only    = true
+}
+
+
+data "azurerm_user_assigned_identity" "identity_prod_ci" {
+  name                = "${local.project}-web-profile-backend-github-ci-identity"
+  resource_group_name = local.identity_resource_group_name
+}
+
+data "azurerm_user_assigned_identity" "identity_prod_cd" {
+  name                = "${local.project}-web-profile-backend-github-cd-identity"
+  resource_group_name = local.identity_resource_group_name
+}
